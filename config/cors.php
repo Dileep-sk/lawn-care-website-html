437b0495ab54a1dc6666d5874f1b4033498a7342
Dileep-sk <dileep@skylineinfosys.com>
2025-09-11 20:07:36 +0530

    Update core

--- config/cors.php
+++ config/cors.php
@@ -2,19 +2,19 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],  // Ensure Sanctum cookie path is included
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],  // Allows all HTTP methods
+    'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://erp.atithibhavan.com'],  // Specify allowed frontend domain
+    'allowed_origins' => ['*'], // For dev, use '*' or specific URL like 'http://localhost:5173'
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],  // Allows all headers
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,  // Set to true for cookies/session authentication
+    'supports_credentials' => false, // true if you're using cookies/session auth
 ];
